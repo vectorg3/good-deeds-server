a1735fb1595262aab2aec9bcc767a31b860811a9
vectorg3 <diglytail@gmail.com>
2023-06-18 21:39:10 +0300

    add friend method updated(checking friendId)

--- src/friends/friends.service.ts
+++ src/friends/friends.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from 'src/users/schemas/user.schema';
@@ -7,10 +7,17 @@ import { User, UserDocument } from 'src/users/schemas/user.schema';
 export class FriendsService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
   async getAll(userId: string) {
-    return (await this.userModel.findById(userId)).friends;
+    return (await (await this.userModel.findById(userId)).populate('friends'))
+      .friends;
   }
 
   async add(userId: string, friendId: string) {
+    if (!friendId.match(/^[0-9a-fA-F]{24}$/)) {
+      throw new ForbiddenException('Пользователь не найден');
+    }
+    if (!(await this.userModel.findById(friendId))) {
+      throw new ForbiddenException('Пользователь не найден');
+    }
     await this.userModel.findByIdAndUpdate(userId, {
       $push: { friends: friendId },
     });
